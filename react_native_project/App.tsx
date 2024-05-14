55a5c03bdc2a7500d09cdb6103bbd6b2c8dc3f13
roll1226 <tomo_yasu_4724@icloud.com>
2024-05-14 18:07:05 +0900

    add: ReactNativePaper

--- react_native_project/App.tsx
+++ react_native_project/App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Appbar, PaperProvider } from "react-native-paper";
 
 export default function App() {
+  const _goBack = () => console.log("Went back");
+
+  const _handleSearch = () => console.log("Searching");
+
+  const _handleMore = () => console.log("Shown more");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={_goBack} />
+        <Appbar.Content title="Title" />
+        <Appbar.Action icon="magnify" onPress={_handleSearch} />
+        <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+      </Appbar.Header>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
