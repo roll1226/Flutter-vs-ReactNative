86091a8e3f4ac7bc80f46519bf991382fde00eb0
roll1226 <tomo_yasu_4724@icloud.com>
2024-06-10 07:23:14 +0900

    feat: scrollView

--- react_native_project/app/(tabs)/index.tsx
+++ react_native_project/app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "@/components/Themed";
 import { useArticle } from "@/hooks/useArticle";
 import { useSearchQiita } from "@/hooks/useSearchQiita";
 import React from "react";
-import { Button, StyleSheet, TextInput } from "react-native";
+import { Button, ScrollView, StyleSheet, TextInput } from "react-native";
 
 export default function TabOneScreen() {
   const { title, setTitle, articles, searchQiitaHandler } = useSearchQiita();
@@ -22,6 +22,25 @@ export default function TabOneScreen() {
         value={title}
         onChangeText={(title) => setTitle(title)}
       />
+      <ScrollView>
+        {articles.map((article, index) => {
+          return (
+            <View key={index}>
+              <View
+                style={styles.separator}
+                lightColor="#eee"
+                darkColor="rgba(255,255,255,0.1)"
+              />
+              <Text>{article.title}</Text>
+              <View
+                style={styles.separator}
+                lightColor="#eee"
+                darkColor="rgba(255,255,255,0.1)"
+              />
+            </View>
+          );
+        })}
+      </ScrollView>
       <Button onPress={() => searchQiitaHandler(title)} title="Button"></Button>
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
