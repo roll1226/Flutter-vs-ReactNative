5e6ca0059692d52eb084b188934f9cbd2ec7b260
roll1226 <tomo_yasu_4724@icloud.com>
2024-06-12 21:20:32 +0900

    feat: style

--- react_native_project/components/ArticleCard.tsx
+++ react_native_project/components/ArticleCard.tsx
@@ -10,19 +10,30 @@ type Props = {
 export const ArticleCard: FC<Props> = ({ article }) => {
   return (
     <View style={styles.card}>
-      <Text>{format(article.created_at, "yyyy/MM/dd")}</Text>
-      <Text>{article.title}</Text>
-      {article.tags.map((tag) => (
-        <Text>{tag.name}</Text>
-      ))}
-      <Text>{article.likes_count}</Text>
-      <Image
-        style={styles.icon}
-        source={{
-          uri: article.user.profile_image_url,
-        }}
-      />
-      <Text>{article.user.id}</Text>
+      <View>
+        <Text>{format(article.created_at, "yyyy/MM/dd")}</Text>
+        <Text>{article.title}</Text>
+        <View style={{ justifyContent: "flex-start", flexDirection: "row" }}>
+          {article.tags.map((tag) => (
+            <Text>{tag.name}</Text>
+          ))}
+        </View>
+      </View>
+      <View style={{ justifyContent: "space-between", flexDirection: "row" }}>
+        <View>
+          <Text>{article.likes_count}</Text>
+        </View>
+
+        <View style={{ alignItems: "flex-end" }}>
+          <Image
+            style={styles.icon}
+            source={{
+              uri: article.user.profile_image_url,
+            }}
+          />
+          <Text>{article.user.id}</Text>
+        </View>
+      </View>
     </View>
   );
 };
