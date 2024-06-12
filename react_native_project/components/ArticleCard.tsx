fdebbd399a4869a2387b07e3c5895343624445e4
roll1226 <tomo_yasu_4724@icloud.com>
2024-06-13 08:35:18 +0900

    feat: link

--- react_native_project/components/ArticleCard.tsx
+++ react_native_project/components/ArticleCard.tsx
@@ -1,40 +1,76 @@
 import { Article } from "@/models/article";
+import { FontAwesome } from "@expo/vector-icons";
 import { format } from "date-fns";
+import { Link } from "expo-router";
 import React, { FC } from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
+import {
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  useColorScheme,
+} from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 type Props = {
   article: Article;
 };
 
 export const ArticleCard: FC<Props> = ({ article }) => {
+  const colorScheme = useColorScheme();
+
   return (
     <View style={styles.card}>
-      <View style={styles.header}>
-        <Text>{format(article.created_at, "yyyy/MM/dd")}</Text>
-        <Text>{article.title}</Text>
-        <View style={styles.tags}>
-          {article.tags.map((tag) => (
-            <Text>{tag.name}</Text>
-          ))}
+      <Link href="/modal">
+        <View style={styles.header}>
+          <View>
+            <Text>{format(article.created_at, "yyyy/MM/dd")}</Text>
+            <Text>{article.title}</Text>
+            <View style={styles.tags}>
+              {article.tags.map((tag) => (
+                <Text>{tag.name}</Text>
+              ))}
+            </View>
+          </View>
         </View>
-      </View>
+      </Link>
 
       <View style={styles.footer}>
-        <View style={styles.like}>
-          <Icon name="heart" size={24} />
-          <Text>{article.likes_count}</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <View style={styles.like}>
+            <FontAwesome
+              name="heart"
+              size={25}
+              color={Colors[colorScheme ?? "light"].text}
+            />
+            <Text>{article.likes_count}</Text>
+          </View>
+
+          <View style={styles.user}>
+            <Image
+              style={styles.icon}
+              source={{
+                uri: article.user.profile_image_url,
+              }}
+            />
+            <Text>{article.user.id}</Text>
+          </View>
         </View>
 
-        <View style={styles.user}>
-          <Image
-            style={styles.icon}
-            source={{
-              uri: article.user.profile_image_url,
-            }}
-          />
-          <Text>{article.user.id}</Text>
+        <View>
+          <TouchableOpacity style={styles.button}>
+            <Link href="/modal" asChild>
+              <Button title="詳細" />
+            </Link>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -68,8 +104,12 @@ const styles = StyleSheet.create({
   },
   header: {},
   footer: {
-    justifyContent: "space-between",
-    alignItems: "flex-end",
-    flexDirection: "row",
+    flexDirection: "column",
+    gap: 12,
+  },
+  button: {
+    borderColor: "#229",
+    borderWidth: 2,
+    borderRadius: 5,
   },
 });
