40424b84d650adf0fe6b4f98a41ca3e415f14b93
roll1226 <tomo_yasu_4724@icloud.com>
2024-06-13 08:58:51 +0900

    change: delete info icon

--- react_native_project/app/(search)/_layout.tsx
+++ react_native_project/app/(search)/_layout.tsx
@@ -1,9 +1,6 @@
 import { useColorScheme } from "@/components/useColorScheme";
-import { FontAwesome } from "@expo/vector-icons";
-import { Link, Stack } from "expo-router";
+import { Stack } from "expo-router";
 import React from "react";
-import { Pressable } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -19,20 +16,6 @@ export default function SearchLayout() {
         name="index"
         options={{
           title: "検索画面",
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? "light"].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
         }}
       />
     </Stack>
