6b73eb8d6432654fec2e2c27bbce4799e1e3ae57
roll1226 <tomo_yasu_4724@icloud.com>
2024-06-22 20:03:06 +0900

    feat: main test

---
+++ flutter_project/test/main_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project/main.dart';
+import 'package:flutter_project/screens/search_screen.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/mockito.dart';
+
+// Mockクライアントを作成するためにMockitoパッケージを使用します。
+// これは、フェイクのHTTPクライアントを作成するために使用します。
+class MockClient extends Mock implements http.Client {}
+
+void main() {
+  group('MyApp', () {
+    testWidgets('SearchScreenがレンダリングされることを確認する', (WidgetTester tester) async {
+      // アプリを構築してフレームをトリガーします。
+      await tester.pumpWidget(const MyApp());
+
+      // SearchScreenが表示されていることを確認します
+      expect(find.byType(SearchScreen), findsOneWidget);
+    });
+  });
+
+  group('SearchScreen', () {
+    testWidgets('正しいUI要素が表示されていることを確認する', (WidgetTester tester) async {
+      final client = MockClient();
+
+      await tester.pumpWidget(MaterialApp(home: SearchScreen(client: client)));
+
+      // 例: AppBarが存在することを確認する
+      expect(find.byType(AppBar), findsOneWidget);
+
+      // 例: 検索入力用のTextFieldが存在することを確認する
+      expect(find.byType(TextField), findsOneWidget);
+    });
+  });
+}
