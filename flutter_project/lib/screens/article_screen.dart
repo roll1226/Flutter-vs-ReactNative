a6ef402c2036a0fb084eef53f6a68b36a5281065
roll1226 <tomo_yasu_4724@icloud.com>
2024-06-23 14:31:09 +0900

    change: test

--- flutter_project/lib/screens/article_screen.dart
+++ flutter_project/lib/screens/article_screen.dart
@@ -3,19 +3,24 @@ import 'package:flutter_project/models/article.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class ArticleScreen extends StatefulWidget {
-  const ArticleScreen({super.key, required this.article});
+  const ArticleScreen({super.key, required this.article, this.controller});
 
   final Article article;
+  final WebViewController? controller;
 
   @override
   State<ArticleScreen> createState() => _ArticleScreenState();
 }
 
 class _ArticleScreenState extends State<ArticleScreen> {
-  late WebViewController controller = WebViewController()
-    ..loadRequest(
-      Uri.parse(widget.article.url),
-    );
+  late WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = widget.controller ?? WebViewController();
+    controller.loadRequest(Uri.parse(widget.article.url));
+  }
 
   @override
   Widget build(BuildContext context) {
