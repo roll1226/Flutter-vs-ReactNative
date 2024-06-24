045f30a2e507672276e7a465fc5e4fc15aa0ce30
roll1226 <tomo_yasu_4724@icloud.com>
2024-06-25 07:12:22 +0900

    fix: test

--- react_native_project/__mocks__/expo-router.ts
+++ react_native_project/__mocks__/expo-router.ts
@@ -1,3 +1,5 @@
+import { ReactDOM } from "react";
+
 // __mocks__/expo-router.js
-export const Link = ({ children }) => children;
+export const Link = ({ children }: { children: ReactDOM }) => children;
 export const useLinkTo = jest.fn();
